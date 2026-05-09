b119f33e195e5b0ed8fbfbad9add2b0987bf4925
Dekiru <dwyl.agency1@gmail.com>
2026-05-10 02:10:44 +0530

    card.jsx

---
+++ src/component/card.jsx
@@ -0,0 +1,10 @@
+function card() {
+    return <div>
+    <div className="card">
+      <h1 className="h1">Dekiru</h1>
+      <p className="p">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt veniam nesciunt iusto facilis beatae neque quas quasi officia eaque ex dicta corporis culpa quisquam consequuntur fugit repellendus, facere reprehenderit expedita.</p>
+    </div>
+  </div>
+}
+ 
+export default card
