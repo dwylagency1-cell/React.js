e58e6410e2a5e5cde70fef22c5b750e959689964
Dekiru <dwyl.agency1@gmail.com>
2026-05-10 02:06:31 +0530

    App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import card from './component/card.jsx'
+import nav from './component/Navbar.jsx'
+function App() {
+  return <div>
+    {nav()}
+    {card()}
+    {card()}
+    {card()}
+
+
+    
+  </div>
+
+}
+export default App
