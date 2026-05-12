84a63a312086cf61790a1f0e71d4fdb1df94f345
Dekiru <dwyl.agency1@gmail.com>
2026-05-12 14:50:31 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import card from './component/card.jsx'
 import nav from './component/Navbar.jsx'
+import squareCards from './component/squarecards.jsx'
 function App() {
   return <div>
     {nav()}
     {card()}
     {card()}
     {card()}
+    {squareCards()}
+
+    
 
 
     
