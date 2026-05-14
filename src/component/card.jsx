e82efff6a999444c0deb396bce3917f3701320c8
Dekiru <dwyl.agency1@gmail.com>
2026-05-14 15:26:04 +0530

    card.jsx

--- src/component/card.jsx
+++ src/component/card.jsx
@@ -1,6 +1,13 @@
 function Card() {
     return <div >
-    <div className='card'></div>
+    <div className='card'>
+      <div className="top">
+        <div className="top_left"></div>
+        <div className="top_right"></div>
+      </div>
+      <div className="mid"></div>
+      <div className="bottom"></div>
+    </div>
   </div>
 }
  
