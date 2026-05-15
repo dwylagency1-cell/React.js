cefeb75bf13f5bc2bfabdc2ec043706b4a5e9e16
Dekiru <dwyl.agency1@gmail.com>
2026-05-15 16:15:27 +0530

    card.jsx

--- src/component/card.jsx
+++ src/component/card.jsx
@@ -1,12 +1,24 @@
+import {Bookmark} from 'lucide-react'
+import amazonlogo from './img/amazon.png'
 function Card() {
     return <div >
     <div className='card'>
       <div className="top">
-        <div className="top_left"></div>
-        <div className="top_right"></div>
+        <div className="top_left">
+          <img className='amazonimg' src={amazonlogo}></img>
+        </div>
+        <div className="top_right">
+          <button className='save'>Save <Bookmark size={20} /></button>
+        </div>
       </div>
-      <div className="mid"></div>
-      <div className="bottom"></div>
+      <div className="mid">
+        <div className="mid_section">
+          <div className="inside_mid"></div>
+        </div>
+      </div>
+      <div className="bottom">
+        
+      </div>
     </div>
   </div>
 }
